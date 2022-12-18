9d476c1aaf32d3b39940aedc298f7c1c78de7047
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2022-12-18 09:33:25 +0000

    Add styling to style-props variation

--- style-props/src/Todo.jsx
+++ style-props/src/Todo.jsx
@@ -1,9 +1,54 @@
-export default function Todo({ title, detail }) {
+export default function Todo({ title, detail, first, last }) {
+  const style = {
+    alignItems: "center",
+    border: "solid 1px black",
+    borderBottom: "none",
+    display: "flex",
+    listStyleType: "none",
+  };
+
+  if (last) style.borderBottom = "solid 1px black";
+
   return (
-    <li style={{ listStyleType: "none" }}>
-      <input type={"checkbox"}></input>
-      <div>{title}</div>
-      <div>{detail}</div>
+    <li style={style}>
+      <button
+        style={{
+          background: "none",
+          border: "none",
+          cursor: "pointer",
+          height: "50px",
+          padding: "0",
+          width: "50px",
+        }}
+      >
+        <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+          <circle
+            cx="50"
+            cy="50"
+            r="15"
+            fill="none"
+            stroke="black"
+            strokeWidth="2px"
+          />
+        </svg>
+      </button>
+      <div style={{ padding: "0.5rem 0" }}>
+        <div
+          style={{
+            fontWeight: "400",
+          }}
+        >
+          {title}
+        </div>
+        <div
+          style={{
+            fontWeight: "200",
+            fontSize: "0.8rem",
+          }}
+        >
+          {detail}
+        </div>
+      </div>
     </li>
   );
 }
--- style-props/src/TodoList.jsx
+++ style-props/src/TodoList.jsx
@@ -3,15 +3,34 @@ import todos from "./todos.json";
 
 export default function TodoList() {
   return (
-    <>
-      <header>
+    <div
+      style={{
+        boxSizing: "border-box",
+        fontFamily: "'Inter', sans-serif",
+        maxWidth: "600px",
+        margin: "0 auto",
+      }}
+    >
+      <header style={{ margin: "1rem", textAlign: "center" }}>
         <h1>Mars Colonisation Project</h1>
       </header>
-      <ol className="list-style-type-none">
-        {todos.map((todo) => (
-          <Todo key={todo.id} title={todo.title} detail={todo.detail} />
+      <ol
+        style={{
+          listStyleType: "none",
+          padding: "0",
+          boxShadow: "3px 3px 3px #999",
+        }}
+      >
+        {todos.map((todo, index) => (
+          <Todo
+            key={todo.id}
+            title={todo.title}
+            detail={todo.detail}
+            first={index === 0}
+            last={index === todos.length - 1}
+          />
         ))}
       </ol>
-    </>
+    </div>
   );
 }
