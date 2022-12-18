4d06d0b2038bbb2983d2b344914654d3cdc17b21
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2022-12-18 10:39:48 +0000

    Remove style prop accidentally left

--- external-stylesheets/src/Todo.jsx
+++ external-stylesheets/src/Todo.jsx
@@ -1,16 +1,7 @@
 export default function Todo({ title, description }) {
   return (
     <li>
-      <button
-        style={{
-          background: "none",
-          border: "none",
-          cursor: "pointer",
-          height: "50px",
-          padding: "0",
-          width: "50px",
-        }}
-      >
+      <button>
         <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
           <circle
             cx="50"
